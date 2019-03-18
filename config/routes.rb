1cc5136d2da019bc4e1154deb2c3d9a82f476103
Alvaro Alvarez <a.jalvarez@hotmail.com>
2019-03-17 19:28:40 -0500

    2.0

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :pins
+  get 'welcome/index'
+  root 'welcome#index'
+  get 'mypins' => 'pins#mypins'
+  get 'pinsof/:user_id' => 'pins#pinsof' , :as => "pinsof"
+  #le mandamos un parametro a pinsof
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
