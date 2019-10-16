f18277315701e173be17d60f9b40169a007b07c0
Alvaro Alvarez <alvaro.alvarez@youse.com.co>
2019-10-16 15:49:21 -0500

    fix bug

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -36,7 +36,6 @@ class PinsController < ApplicationController
   def create
     @pin = Pin.new(pin_params)
     @pin.user_id = current_user.id
-    byebug
     respond_to do |format|
       if @pin.save
         format.html { redirect_to @pin, notice: 'Pin was successfully created.' }
@@ -80,6 +79,6 @@ class PinsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:photo, :description,:name,:user_id)
+      params.require(:pin).permit(:photo, :description,:name )
     end
 end
